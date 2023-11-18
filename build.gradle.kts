8b960a639e19613d961903ce25b8e5037d9e1af8
kubra <kubracetin.ist@gmail.com>
2023-11-18 17:25:58 -0500

    added plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+
+
+    // Add Kotlin Serialization plugin
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10" apply false
 }
